0c74e2f7a0599ea9b886c7d1e328521cc9c0d429
andreyfesunov <117005912+andreyfesunov@users.noreply.github.com>
2025-08-13 17:54:59 +0300

    feat(lc326): add solution

---
+++ python/leetcode/326.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/power-of-three/
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        arr = [1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147, 531441, 1594323, 4782969, 14348907, 43046721, 129140163, 387420489, 1162261467]
+
+        return n in arr
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while n % 3 == 0:
+            n //= 3
+        return n == 1
